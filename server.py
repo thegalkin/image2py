d4d31943279819f9607847d5a7f988da88173b30
theGalkin <53794828+thegalkin@users.noreply.github.com>
2020-06-21 13:34:34 +0300

    --- Auto-Git Commit ---

--- server.py
+++ server.py
@@ -1,4 +1,14 @@
 import cv2
 import pytesseract
+import os
+from pathlib import Path
 pytesseract.pytesseract.tesseract_cmd = "/usr/local/Cellar/tesseract/4.1.1/bin/tesseract"
-img = 
+
+img = cv2.imread("Import/image.jpg")
+text = pytesseract.image_to_string(img)
+f = open("output.py", "w+")
+f.write(text)
+#os.system(programm)
+f.close()
+os.rmdir("Import")
+os.mkdir("Import")
